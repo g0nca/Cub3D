13b80586bcb0bf2110dc441d952c0148cc09ba16
Gonca <ggomes-v@c2r2s3.42porto.com>
2025-11-24 13:44:29 +0000

    init file with norm done

--- src/init/init_game.c
+++ src/init/init_game.c
@@ -6,7 +6,7 @@
 /*   By: ggomes-v <ggomes-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 15:53:48 by ggomes-v          #+#    #+#             */
-/*   Updated: 2025/11/24 12:04:06 by ggomes-v         ###   ########.fr       */
+/*   Updated: 2025/11/24 13:43:17 by ggomes-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,39 +55,41 @@ void	init_weapon_assets(t_game *game)
 	game->weapon.is_firing = 0;
 	game->weapon.current_frame = 0;
 }
-static void     init_keys(t_game *game)
-{
-    int i;
 
-    i = 0;
-    while (i < 256)
-    {
-        game->keys[i] = 0;
-        i++;
-    }
-}
-void    init_game(t_game *game)
+static void	init_keys(t_game *game)
 {
-    game->mlx = mlx_init();
-    if (!game->mlx)
-        print_error_and_exit_FREE("mlx_init failed", 1, NULL);
-    game->win = mlx_new_window(game->mlx, WIN_W, WIN_H, "Cub3D");
-    if (!game->win)
-        print_error_and_exit_FREE("mlx_init failed", 1, NULL);
-    game->screen.img = mlx_new_image(game->mlx, WIN_W, WIN_H);
-    if (!game->screen.img)
-        print_error_and_exit_FREE("mlx_init failed", 1, NULL);
-    game->screen.addr = mlx_get_data_addr(game->screen.img,
-        &game->screen.bpp, &game->screen.line_len, &game->screen.endian);
-    init_images(game);
-    init_weapon_assets(game);
-    game->win_game = 0;
-    game->player.x = 0.0;
-    game->player.y = 0.0;
-    game->player.angle = 0.0;
-    game->player.move_speed = 0.02;
-    game->player.rot_speed = 0.01;
-    init_keys(game);
-    game->key_left = 0;
-    game->key_right = 0;
+	int	i;
+
+	i = 0;
+	while (i < 256)
+	{
+		game->keys[i] = 0;
+		i++;
+	}
+}
+
+void	init_game(t_game *game)
+{
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		print_error_and_exit_FREE("mlx_init failed", 1, NULL);
+	game->win = mlx_new_window(game->mlx, WIN_W, WIN_H, "Cub3D");
+	if (!game->win)
+		print_error_and_exit_FREE("mlx_init failed", 1, NULL);
+	game->screen.img = mlx_new_image(game->mlx, WIN_W, WIN_H);
+	if (!game->screen.img)
+		print_error_and_exit_FREE("mlx_init failed", 1, NULL);
+	game->screen.addr = mlx_get_data_addr(game->screen.img,
+			&game->screen.bpp, &game->screen.line_len, &game->screen.endian);
+	init_images(game);
+	init_weapon_assets(game);
+	game->win_game = 0;
+	game->player.x = 0.0;
+	game->player.y = 0.0;
+	game->player.angle = 0.0;
+	game->player.move_speed = 0.02;
+	game->player.rot_speed = 0.01;
+	init_keys(game);
+	game->key_left = 0;
+	game->key_right = 0;
 }
--- src/init/load_textures.c
+++ src/init/load_textures.c
@@ -3,27 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   load_textures.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ggomes-v <ggomes-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 15:53:49 by ggomes-v          #+#    #+#             */
-/*   Updated: 2025/11/12 11:03:21 by joaomart         ###   ########.fr       */
+/*   Updated: 2025/11/24 13:44:13 by ggomes-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static void load_texture(void *mlx, t_img *img, char *path)
+static void	load_texture(void *mlx, t_img *img, char *path)
 {
-    img->img = mlx_xpm_file_to_image(mlx, path, &img->width, &img->height);
-    if (!img->img)
-        print_error_and_exit_FREE("mlx_init failed", 1, NULL);
-    img->addr = mlx_get_data_addr(img->img, &img->bpp, &img->line_len, &img->endian);
+	img->img = mlx_xpm_file_to_image(mlx, path, &img->width, &img->height);
+	if (!img->img)
+		print_error_and_exit_FREE("mlx_init failed", 1, NULL);
+	img->addr = mlx_get_data_addr(img->img, &img->bpp,
+			&img->line_len, &img->endian);
 }
 
-void    init_images(t_game *g)
+void	init_images(t_game *g)
 {
-    load_texture(g->mlx, &g->textures.north, g->map.no_texture);
-    load_texture(g->mlx, &g->textures.south, g->map.so_texture);
-    load_texture(g->mlx, &g->textures.west, g->map.we_texture);
-    load_texture(g->mlx, &g->textures.east, g->map.ea_texture);
+	load_texture(g->mlx, &g->textures.north, g->map.no_texture);
+	load_texture(g->mlx, &g->textures.south, g->map.so_texture);
+	load_texture(g->mlx, &g->textures.west, g->map.we_texture);
+	load_texture(g->mlx, &g->textures.east, g->map.ea_texture);
 }
